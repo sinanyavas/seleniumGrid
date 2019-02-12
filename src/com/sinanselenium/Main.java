83bcc3418b9dca80a10845f9e4fe2f268e27863d
“sinanyavas” <sinanyavas@gmail.com>
2019-02-12 21:49:36 +0000

    seleniumGrid

---
+++ src/com/sinanselenium/Main.java
@@ -0,0 +1,28 @@
+package com.sinanselenium;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) throws MalformedURLException {
+        // TODO Auto-generated method stub
+        //Automated- chrome,firefox,ie,safari
+
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setBrowserName("chrome");
+        dc.setPlatform(Platform.WINDOWS);
+        //dc.se
+
+        //Webdriver driver=new ChromeDriver();
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
+        driver.get("http://rediff.com");
+
+        //
+    }
+}
